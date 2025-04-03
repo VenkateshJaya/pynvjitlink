00acfdeb2e5c728562bc6d7236d78bd76d8b9c1c
VenkateshJaya <jjayabaskar@nvidia.com>
2025-04-03 16:18:28 -0700

    removed wheel_dir variable from ci scripts

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 
 source rapids-configure-sccache
 
-wheel_dir=${RAPIDS_WHEEL_BLD_OUTPUT_DIR}
-
 rapids-logger "Install CUDA Toolkit"
 source "$(dirname "$0")/install_latest_cuda_toolkit.sh"
 
@@ -21,7 +19,7 @@ rapids-pip-retry wheel . --wheel-dir=./dist -v --disable-pip-version-check --no-
 sccache --show-adv-stats
 
 # Exclude libcuda.so.1 because we only install a driver stub
-python -m auditwheel repair --exclude libcuda.so.1 -w "${wheel_dir}" ./dist/*
+python -m auditwheel repair --exclude libcuda.so.1 -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" ./dist/*
 
 rapids-logger "Upload Wheel"
-RAPIDS_PY_WHEEL_NAME="pynvjitlink_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python "${wheel_dir}"
+RAPIDS_PY_WHEEL_NAME="pynvjitlink_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
